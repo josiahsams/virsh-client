be47365ac83a3d813ae9d14823523dec95dd098f
Josiah Samuel <josiah_sams@in.ibm.com>
2021-02-12 14:10:49 +0530

    minor patch

--- prepareImgV3.sh
+++ prepareImgV3.sh
@@ -11,21 +11,13 @@ usage()
                   [ -z | --zpdt   FILEPATH.tar.gz   ] BASE_IMG"
   exit 2
 }
-ld=$(losetup -f)
 
 # unmount dir & remove tmp files
 cleanup() {
     trap - EXIT
     echo "Cleanup .."
-    kpartx -d ${ld}
-losetup -d ${ld}
-
     umount ${MNT_POINT}/proc
-#    umount ${MNT_POINT}/sys
-#    umount ${MNT_POINT}/dev/pts
-#    umount ${MNT_POINT}/dev
-
- umount -f -l ${MNT_POINT} >/dev/null 2>&1
+    umount -f -l ${MNT_POINT} >/dev/null 2>&1
     rmdir ${MNT_POINT} >/dev/null 2>&1
     echo "done"
     exit 0
@@ -83,6 +75,16 @@ if [ ! ${ZPDT##*.} = "gz" ]; then
     exit 1
 fi
 
+# Check key.pem is populated with private keys
+file='./key.pem'
+if [ -s "$file" ]
+then 
+   echo "found key.pem"
+else
+   echo "key.pem does not exist, or is empty "
+   exit 1
+fi
+
 if [[ ! ${BASE_IMG##*.} = "img" && ! ${BASE_IMG##*.} == "qcow2" ]]; then
     echo "BASE_IMG : $BASE_IMG is not the expected one"
     exit 1
@@ -101,53 +103,18 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# image resized for workspace
-# qemu-img resize ${BASE_IMG} 4G
-# if [ $? -ne 0 ]; then
-#     echo "qemu-img failed"
-#     exit 1
-# fi
-
 mkdir -p ${MNT_POINT}
 
-# Convert cloud image to raw
+# Convert cloud image to raw & resize it to 4G
 qemu-img convert -O raw ${BASE_IMG} ${BASE_IMG}.tmp.raw || exit 1
 qemu-img create -f raw ${BASE_IMG}.raw 4G
 virt-resize ${BASE_IMG}.tmp.raw ${BASE_IMG}.raw --expand /dev/sda1
-
 rm ${BASE_IMG}.tmp.raw
-
-#fallocate ${BASE_IMG}.raw -l 4G
 
 # get partition details & mount it
 start=`fdisk -l ${BASE_IMG}.raw  | tail -1 | awk '{ print $3 }'`
 startoffset=`expr $start \* 512`
 mount -o loop,offset=${startoffset}  ${BASE_IMG}.raw ${MNT_POINT} || exit 1
-
-# Get the corresponding loop device and initiate a FS resize.
-#loopDev=`df -h | grep ${MNT_POINT} | awk '{ print $1 }'`
-#resize2fs $loopDev
-# e2fsck -y $loopDev
-
-#qemu-img info ${BASE_IMG}.raw
-#losetup $ld ${BASE_IMG}.raw
-#resize2fs $ld
-#e2fsck -y $ld
-
-# kpartx -a $ld
-#
-# echo "ld : ${ld}"
-# ls -lt /dev/mapper/
-# ldbase=`basename $ld`
-# ldpartition=`ls -lt /dev/mapper/ | grep $ldbase | awk '{ print $9 }'`
-# ldpartition="/dev/mapper/${ldpartition}"
-# #e2fsck -f $ldpartition
-# #e2fsck -y $ldpartition
-# #resize2fs $MNT_POINT
-# #e2fsck -y $ldpartition
-#
-# echo "ldpartition : ${ldpartition}"
-# mount $ldpartition  ${MNT_POINT}
 
 # place all the runz binaries
 cp -r ${RUNZ_BIN}/* ${MNT_POINT}/ || exit 1
@@ -182,10 +149,6 @@ grep -q "$entry" ${MNT_POINT}/etc/security/limits.conf || echo "$entry" >> ${MNT
 entry='mount /dev/vdc /volumes'
 grep -q "$entry" ${MNT_POINT}/etc/fstab || echo "$entry" >> ${MNT_POINT}/etc/fstab
 
-# set sudo access to runz
-# entry='runz  ALL=(ALL) NOPASSWD: ALL'
-# grep -q "$entry" ${MNT_POINT}/etc/sudoers || echo "$entry" >> ${MNT_POINT}/etc/sudoers
-
 # extract kernel patches
 mkdir -p ${MNT_POINT}/patches
 tar -C ${MNT_POINT}/patches -zxf ${KERNEL} || exit 1
@@ -197,13 +160,6 @@ echo "PATCHDIR: ${PATCHDIR}"
 echo "KVER: ${KVER}"
 
 mount -t proc -o nosuid,noexec,nodev proc ${MNT_POINT}/proc
-# mount -t sysfs -o nosuid,noexec,nodev sysfs ${MNT_POINT}/sys
-# mount -o bind /dev ${MNT_POINT}/dev/
-# mkdir -p -m 755 ${MNT_POINT}/dev/pts
-# mount -t devtmpfs -o mode=0755,nosuid devtmpfs ${MNT_POINT}/dev
-# mount -t devpts -o gid=5,mode=620 devpts ${MNT_POINT}/dev/pts
-
-# chroot ${MNT_POINT} update-initramfs -u -k all
 
 KERNEL_SHORTID=`echo ${KVER} | awk -F'-' '{ print $1 }'`
 echo "KERNEL_SHORTID : ${KERNEL_SHORTID}"
@@ -249,11 +205,6 @@ chroot ${MNT_POINT} /bin/bash <<EOT
     # create a soft link for volumes
     ln -sf /volumes /container/volumes
 EOT
-
-umount ${MNT_POINT}/proc
-# umount ${MNT_POINT}/sys
-# umount ${MNT_POINT}/dev/pts
-# umount ${MNT_POINT}/dev
 
 sync
 
